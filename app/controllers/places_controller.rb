c784cccf46ed416f2288973b40c9a612ba7bcd6b
Jacob Petel <Jacob.Petel@outlook.com.com>
2019-07-10 04:19:06 +0000

    Editing works

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -24,6 +24,11 @@ end
   @place = Place.find(params[:id])
  end
 
+ def update
+  @place = Place.find(params[:id])
+  @place.update_attributes(place_params)
+  redirect_to root_path
+end
 
 
 private
