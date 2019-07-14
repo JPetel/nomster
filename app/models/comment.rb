8791e9f7df92468a34b3596d3da743f68e9d6034
Jacob Petel <Jacob.Petel@outlook.com.com>
2019-07-14 18:43:26 +0000

    comment testing

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,17 @@
 class Comment < ApplicationRecord
     belongs_to :user
     belongs_to :place
+
+    RATINGS = {
+      'Five Stars': '5_stars',
+      'Four Stars': '4_stars',
+      'Three Stars': '3_stars',
+      'Two Stars': '2_stars',
+      'One Star': '1_stars',
+    }
+
+    def humanized_rating
+      RATINGS.invert[self.rating]
+    end
+
 end
